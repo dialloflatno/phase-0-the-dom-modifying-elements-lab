f60174b32eb639c8a891c69376eb3bdeac3e73df
dialloflatno <brahimdiallo23@gmail.com>
2021-10-05 11:12:40 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
-// Write your code here!
+const main = document.querySelector(`main`)
+
+const newHeader = document.createElement('h1')
+newHeader.setAttribute("id","victory")
+newHeader.innerHTML=("YOUR-NAME is the champion")
